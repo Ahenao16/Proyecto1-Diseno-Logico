e2f42ac6bd75a2458cc72f78b2e8b8ccf14f4626
Ahenao16 <ahenao@estudiantec.cr>
2025-03-23 00:03:15 -0600

    [ADD:Paridad Impar]

--- HammingCode.py
+++ HammingCode.py
@@ -19,45 +19,66 @@ class Interfaz:
         self.cantidad_BitsParidad = 0
         self.matriz_paridad = []
         self.numero_con_paridad = ""
-        self.numero_con_error =""
+        self.numero_con_error ="" 
         self.bit_error = ""
 
-        self.frame = tk.Frame(self.Ventana)  
-        self.frame.place(relx=0.5, rely=0.5, anchor="center")
-        self.frame.configure(background='#E9E0D6')
-        self.label_instrucciones = tk.Label(self.frame, text="Ingrese el número en hexadecimal y presione el botón. Este debe estar formado por 3 caracteres válidos (En el caso de ser un número de dos o un dígito complete el faltante con ceros). Los caracteres numéricos deben ser colocados en minúscula", font=("Arial", 12), fg="Black", anchor = "center",  wraplength=700, bg="#E9E0D6")
-        self.label_instrucciones.pack(pady=10)
+  
+        self.canvas = tk.Canvas(self.Ventana, bg="#E9E0D6")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        
+        self.scrollbar = tk.Scrollbar(self.Ventana, orient="vertical", command=self.canvas.yview)
+        self.scrollbar.pack(side="right", fill="y")
+        
+    
+        self.canvas.config(yscrollcommand=self.scrollbar.set)
+
+  
+        self.frame = tk.Frame(self.canvas, bg="#E9E0D6")
+        self.canvas.create_window((0, 0), window=self.frame, anchor="nw")
+
+      
+        self.frame.bind("<Configure>", self.on_frame_configure)
+
+   
+        self.label_instrucciones = tk.Label(self.frame, text="Ingrese el número en hexadecimal y presione el botón. Este debe estar formado por 3 caracteres válidos (En el caso de ser un número de dos o un dígito complete el faltante con ceros). Los caracteres numéricos deben ser colocados en minúscula", font=("Arial", 12), fg="Black", anchor="center", wraplength=700, bg="#E9E0D6")
+        self.label_instrucciones.pack(pady=10, padx=50)
 
         self.entry_numero = tk.Entry(self.frame, font=("Arial", 12))
-        self.entry_numero.pack(pady=10)
+        self.entry_numero.pack(pady=10, padx=50)
 
-        self.Boton_conversion = tk.Button(self.frame, text="Presionar", command=self.Comprobacion_numero)
-        self.Boton_conversion.pack()
+        self.Boton_conversion = tk.Button(self.frame, text="Presionar", command=self.Comprobacion_numero ,highlightthickness=2,bd=2, bg="white")
+        self.Boton_conversion.pack(pady=10, padx=50)
 
         self.label_error = tk.Label(self.frame, text="", font=("Arial", 12), fg="#F25757", bg="#E9E0D6")
-        self.label_error.pack(pady=10)
+        self.label_error.pack(pady=10, padx=50)
 
-        self.tabla_numeros = ttk.Treeview(self.frame,columns = ('Binario', 'Octal', 'Decimal'), show="headings", height=1, selectmode="none" )
+        self.tabla_numeros = ttk.Treeview(self.frame, columns=('Binario', 'Octal', 'Decimal'), show="headings", height=1, selectmode="none")
         self.tabla_numeros.heading('Binario', text='Binario (12 bits)')
         self.tabla_numeros.heading('Octal', text='Octal')
         self.tabla_numeros.heading('Decimal', text='Decimal')
-        self.tabla_numeros.column('Binario', width=200, anchor='center')  
-        self.tabla_numeros.column('Octal', width=200, anchor='center')    
-        self.tabla_numeros.column('Decimal', width=200, anchor='center')  
-        self.tabla_numeros.pack(pady=10)
+        self.tabla_numeros.column('Binario', width=200, anchor='center')
+        self.tabla_numeros.column('Octal', width=200, anchor='center')
+        self.tabla_numeros.column('Decimal', width=200, anchor='center')
+        self.tabla_numeros.pack(pady=10, padx=50)
 
         self.formato_tabla_numeros = ttk.Style()
-        self.formato_tabla_numeros.configure("Treeview", 
-                             font=("Arial", 12),   
-                             background="#dcdcdc", 
-                             rowheight=25)
-
-        self.formato_tabla_numeros.configure("Treeview.Heading", 
-                             font=("Arial", 12, "bold"))
+        self.formato_tabla_numeros.configure("Treeview", font=("Arial", 12), background="#dcdcdc", rowheight=25)
+        self.formato_tabla_numeros.configure("Treeview.Heading", font=("Arial", 12, "bold"))
         
-    
         self.frame_NRZI = tk.Frame(self.frame)
-        self.frame_NRZI.pack(pady=20)
+        self.frame_NRZI.pack(pady=5, padx=50)
+
+        self.label_eleccion_paridad = tk.Label(self.frame, text="", font=("Arial", 12), bg="#E9E0D6")
+        self.label_eleccion_paridad.pack(pady=5, padx=50)
+
+        self.boton_paridad_par= tk.Button(self.frame, text="", font=("Arial", 12), bg="#E9E0D6",bd=0, highlightthickness=0,command=lambda:self.Llamada_paridad_par())
+        self.boton_paridad_par.pack(side="left", padx=185,pady=5)
+
+        self.boton_paridad_impar= tk.Button(self.frame, text="", font=("Arial", 12), bg="#E9E0D6",bd=0, highlightthickness=0,command=lambda:self.Llamada_paridad_impar())
+        self.boton_paridad_impar.pack(side="left")
+
+    def on_frame_configure(self, event=None):
+        self.canvas.config(scrollregion=self.canvas.bbox("all"))
 
     def Comprobacion_numero(self):
         diccionario = self.Diccionario_valido
@@ -83,11 +104,19 @@ class Interfaz:
         print(self.matriz_paridad)
         print(self.numero_con_paridad)
         print(len(self.numero_con_paridad))
+
+    def Llamada_paridad_par(self):
         self.Paridad_par(self.numero_con_paridad)
         self.Mostrar_bits_paridad()
         print(self.numero_con_paridad)
-        self.Revisar_error_par("00000101001000101")
+        self.Revisar_error_par("00010101001010101")
         print(self.bit_error)
+
+    def Llamada_paridad_impar(self):
+        self.Paridad_impar(self.numero_con_paridad)
+        self.Mostrar_bits_paridad()
+        print(self.numero_con_paridad)
+      
 
     def Obtener_numero(self):
         self.numero_final = self.entry_numero.get()
@@ -136,6 +165,12 @@ class Interfaz:
         canvas = FigureCanvasTkAgg(fig, master=self.frame_NRZI)
         canvas.draw()
         canvas.get_tk_widget().pack()
+        self.label_eleccion_paridad.config(text="Eliga la paridad que desea aplicar a los datos binarios")
+        self.boton_paridad_par.config(text="Paridad Par",highlightthickness=2,bd=2, bg="white" )
+        self.boton_paridad_impar.config(text="Paridad Impar",highlightthickness=2,bd=2, bg="white")
+     
+       
+        
 
     def Calculo_bits_paridad(self, cadena_bits):
             p=0
@@ -150,7 +185,7 @@ class Interfaz:
                 num = i
                 for j in range(bits_paridad): 
                     fila.insert(0, num % 2)  #Obtengo el bit menos significativo (residuo al dividir la representacion decimal entre 2)
-                    num //= 2  #Division entera para obtener el sigiente numero que hay que dicidir
+                    num //= 2  #Division entera para obtener el sigiente numero que hay que dividir
                 matriz.append(fila)  
             
             self.matriz_paridad = self.Transponer(matriz)
@@ -211,7 +246,27 @@ class Interfaz:
         self.numero_con_paridad = ''.join(lista_binario)
 
 
-    #Arreglar checkeo de paridad
+    def Paridad_impar(self, numero_binario):
+        lista_binario = list(numero_binario)
+        for n in range(1, self.cantidad_BitsParidad + 1):
+            posicion_paridad = 2**(n-1) - 1  
+
+            posiciones_paridad = getattr(self, f"Posiciones_p{n}")  
+            contador_unos = 0
+            
+            for pos in posiciones_paridad:
+                if pos < len(lista_binario) and lista_binario[pos] == "1":
+                    contador_unos += 1
+            if contador_unos % 2 == 0:
+                paridad = 0
+            else:
+                paridad = 1
+
+            lista_binario[posicion_paridad] = str(paridad)
+            setattr(self, f"p{n}", paridad)  
+        self.numero_con_paridad = ''.join(lista_binario)
+
+
     def Revisar_error_par(self, numero_binario):
         lista_binario = list(numero_binario)
         bit_error_posicion = 0  
@@ -231,7 +286,6 @@ class Interfaz:
             else:
                 paridad = 0
 
-            
             if posicion_paridad < len(lista_binario) and int(lista_binario[posicion_paridad]) != paridad:
                 error_posicion_binario.append('1') 
             else:
